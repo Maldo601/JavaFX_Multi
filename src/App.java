f72ae17f689a4dd6b43d54c165df2dbc8cab9315
Maldo601 <maldo601@gmail.com>
2021-01-29 02:15:52 +0100

    Masters

--- src/App.java
+++ src/App.java
@@ -3,7 +3,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+/*  
+    Instructions ( by Maldo )
+    Download lib, set environmen variable for path lib sfx 14.0.2, go 
+    cd src folder App, execute nexT commands in terminal: 
 
+    For Compile: 
+    javac --module-path "<PATH>" --add-modules javafx.conttols <nam jav file>.jav
+    For run APP :
+    Same but "java" ( no javac )
+
+    ++more info in: 
+    https://www.youtube.com/watch?v=I4J5_IWDeEo 
+    https://openjfx.io/openjfx-docs/#install-javafx 
+
+*/
 public class App extends Application {
 
     @Override
@@ -18,7 +32,6 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
-
+// M A L D O // 
 
